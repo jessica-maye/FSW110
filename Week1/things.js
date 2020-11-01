25fdd8a30f7886481204d5c5c689f753c1a1cf08
jessica-maye <65079252+jessica-maye@users.noreply.github.com>
2020-10-31 20:26:24 -0400

    caterpillars

---
+++ Week1/things.js
@@ -0,0 +1,16 @@
+//combine all three 
+var things = document.getElementsByClassName("things")
+console.log(things) 
+
+
+//make an array by putting a bracket and 0
+//var things = document.getElementsByClassName("things")
+//console.log(things[0]) 
+
+//log to the console 
+//var things = document.getElementsByClassName("things")
+//for (var i = 0; i < things.length; i++) {
+//   console.log(things[i].innerText)}
+
+
+
