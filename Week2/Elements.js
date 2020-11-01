362eefa8b8756d39eb9cbbc36ae0b0e25e504b57
jessica-maye <65079252+jessica-maye@users.noreply.github.com>
2020-10-31 21:28:24 -0400

    Elements

---
+++ Week2/Elements.js
@@ -0,0 +1,22 @@
+//end of text
+var newLi = document.createElement("li")
+
+newLi.textContent = "These are the top caterpillars in NY"
+
+var myList1 = document.getElementById("my-list1")
+myList1.append(newLi)
+
+
+//New H1 text with text 
+var newH1 =document.createElement("H1")
+newH1.textContent = "Welcome to my JS site!"
+document.body.prepend(newH1)
+
+//New paragraph with text
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript!"
+document.body.append(newP)
+
+//style the text
+newP.style.textAlign = "center"
+newP.style.fontSize = "30px"
