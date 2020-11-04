c99945fccdbb84e39ca6591b89bb8c0dc50076bb
jessica-maye <65079252+jessica-maye@users.noreply.github.com>
2020-11-04 10:56:39 -0500

    Array

--- week3/array.js
+++ week3/array.js
@@ -1,3 +1,4 @@
+//H1 tag with for loop
 var newH1 = document.createElement("H1")
 newH1.textContent = "Hello World"
 document.body.append(newH1)
@@ -10,4 +11,18 @@ document.body.append(newH1)
     newH1.style.color = "red"
 }
 
+//create a numhered list
+var list1 = document.createElement("ol")
+list1.textContent = "A list"
+document.body.append(list1)
 
+
+//add a list of names inside a for loop
+var names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"]
+
+
+for(var i = 0; i < names.length; i++) {
+    var newNames = document.createElement("li")
+    newNames.textContent = names[i]
+    list1.append(newNames)
+}
